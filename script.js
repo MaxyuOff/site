2c17b41581103e65a2aa3a303fa9bb1669fca192
Maxyu Guzeevgeny <103492719+MaxyuOff@users.noreply.github.com>
2022-10-27 07:24:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const windowInnerHeight = document.documentElement.clientHeight;
-//style.section.height = windowInnerHeight + "px";
-let doc = document.querySelectorAll('section');
-Window.print(doc)
+//section.style.height = windowInnerHeight + "px";
+//var doc = document.querySelectorAll('section');
+Window.print(windowInnerHeight)
 //doc.style.Height = windowInnerHeight + "px";
